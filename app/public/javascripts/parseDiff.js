c6e643a8b7e383894994a07243eb896af65ed013
phillip <phillip.schmidt@whiteboard-it.com>
2013-06-26 02:14:20 -0500

    only get one filename

--- app/public/javascripts/parseDiff.js
+++ app/public/javascripts/parseDiff.js
@@ -2,7 +2,7 @@ function parseDiff(fileArray) {
 	var fileSelectHtml = ""
     , diffHtml = "";
 	diffText.forEach(function(file) {
-    var fileNameLine = file.lines[0];
+    var fileNameLine = file.lines[0].split(' ')[0];
     fileSelectHtml +=  '<div class="' + file.type + '"></div><div class="filePath">' + fileNameLine.substring(fileNameLine.indexOf('\/'),fileNameLine.length -1) + '</div>'
     diffHtml += '<ul class="fileLines">';
     file.lines.forEach(function(line) {
